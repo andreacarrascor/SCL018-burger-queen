171372469a943d1f3f1fe3769657009b4e0f9a37
andreacarrascor <ancarami19@gmail.com>
2022-01-10 17:45:19 -0300

    Ruteo sección waiter y kitchen, instalación Tailwindcss

---
+++ src/componentes/Kitchen.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Kitchen = () => {
+    return (
+        <>
+            <div>
+                <h1>aquí va Kitchen</h1>
+            </div>
+        </>
+    );
+};
+
+export default Kitchen;
