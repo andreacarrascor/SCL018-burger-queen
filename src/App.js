7092343e8923b50ff4acfa512ab69b0329df7543
andreacarrascor <ancarami19@gmail.com>
2022-01-19 18:16:11 -0300

    contexto global para agregar productos a la orden

--- src/App.js
+++ src/App.js
@@ -1,20 +1,76 @@
+/* eslint-disable no-unused-vars */
+import React, {useContext, useState} from "react";
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import React from "react";
+import menu from "./menu.json";
 import Home from './componentes/Home';
 import Waiter from './componentes/Waiter';
 import Kitchen from './componentes/Kitchen';
-import { Route, Routes } from 'react-router-dom';
 
-
+const dataMenu = menu;
+console.log(dataMenu);
+export const ContextProducts = React.createContext();
 
 const App = () => {
+  const [products, setproducts] = useState({
+    menuArrays: dataMenu,
+    resumeOrder: []
+  });
+
+
+  const addProducts = (dish) => {
+    return setproducts({
+      ...products,
+      resumeOrder: products.resumeOrder.find((item) => item.id === dish.id)
+        ? products.resumeOrder.map((item) =>
+            item.id === dish.id
+              ? { ...item, count: item.count + 1 }
+              : item
+          )
+        : [...products.resumeOrder, { ...dish, count: 1 }]
+    });
+  }
+
+  const removeFromOrder = (id) => {
+    setproducts({
+      ...products,
+      resumeOrder: products.resumeOrder.filter((item) => item.id !== id)
+    });
+  };
+  
+  const increase = (id) => {
+    setproducts({
+      ...products,
+      resumeOrder: products.resumeOrder.map((item) =>
+        item.id === id
+          ? { ...item, count: item.count + 1 }
+          : item
+      )
+    });
+  };
+
+  const decrease = (id) => {
+    setproducts({
+      ...products,
+      resumeOrder: products.resumeOrder.map((item) =>
+        item.id === id
+          ? { ...item, count: item.count > 1 ? item.count - 1 : 1 }
+          : item
+      )
+    });
+  };
+
+  const resumeProps = { products, addProducts, removeFromOrder, increase, decrease };
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/waiter" element={<Waiter />} />
-        <Route path="/kitchen" element={<Kitchen />} />
-      </Routes>
+      <ContextProducts.Provider value={resumeProps}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/waiter" element={<Waiter />} />
+          <Route path="/kitchen" element={<Kitchen />} />
+        </Routes>
+      </ContextProducts.Provider>
     </>
 
   );
--- src/componentes/Drinks.js
+++ src/componentes/Drinks.js
@@ -1,18 +1,20 @@
-import React from "react";
-import menu from "../menu.json";
+import React, {useContext} from "react";
+import {ContextProducts} from "../App.js";
 
-const Drinks = () => {
-    const menuDrinks = menu.drinks;
+
+const Drinks = (props) => {
+    const globalContext = useContext(ContextProducts);
+    const menuDrinks = globalContext.products.menuArrays.drinks
     return (
         <>
             <h2 className="font-bold m-5">Calientes y Frías</h2>
-            {menuDrinks.map((e, index) => {
+            {menuDrinks.map((drink) => {
                 return (
                     <button 
                         className="flex border-solid border-2 border-teal-500 rounded-md space-x-4 m-2 ml-5 p-2 font-semibold text-gray-500 bg-none"
-                        // onClick={} 
-                        key={index}>
-                            <p className="whitespace-pre">{e.name}    ${e.price}</p>
+                        onClick={() => globalContext.addProducts(drink)}
+                        key={drink.id}>
+                            <p className="whitespace-pre">{drink.name}    ${drink.price}</p>
                     </button>
                     
                 );
